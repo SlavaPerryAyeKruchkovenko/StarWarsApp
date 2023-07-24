07a5de6d73bcaf9bfe28738edc5467dc98b7edd7
Slava <kruchkovenko@gmail.com>
2023-07-24 21:50:29 +0500

    add starship model

--- app/src/main/java/com/example/starwarsapp/data/interfaces/IStarship.kt
+++ app/src/main/java/com/example/starwarsapp/data/interfaces/IStarship.kt
@@ -2,10 +2,11 @@ package com.example.starwarsapp.data.interfaces
 
 import com.example.starwarsapp.data.models.Movie
 
-interface ICharacter {
+interface IStarship {
     val id: String
     val name: String
-    val sex: String
-    val starshipsCount: Int
+    val model: String
+    val manufacturer: String
+    val pilots: Array<String>
     val films: Array<Movie>
 }
--- app/src/main/java/com/example/starwarsapp/data/models/Starship.kt
+++ app/src/main/java/com/example/starwarsapp/data/models/Starship.kt
@@ -1,16 +1,17 @@
 package com.example.starwarsapp.data.models
 
-import com.example.starwarsapp.data.interfaces.ICharacter
+import com.example.starwarsapp.data.interfaces.IStarship
 import com.example.starwarsapp.data.interfaces.Likeable
 
-data class Character(
+data class Starship(
     override val id: String,
     override val name: String,
-    override val sex: String,
-    override val starshipsCount: Int,
+    override val model: String,
+    override val manufacturer: String,
+    override val pilots: Array<String>,
     override val films: Array<Movie>,
     private val isLiked: Boolean = false
-) : StarWarsObject(id), ICharacter, Likeable {
+) : StarWarsObject(id), IStarship, Likeable {
     var isLike = isLiked
         private set
 
@@ -25,13 +26,15 @@ data class Character(
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
-        return id == (other as Character).id
+        return id == (other as Starship).id
     }
 
     override fun hashCode(): Int {
         var result = id.hashCode()
         result = 31 * result + name.hashCode()
-        result = 31 * result + sex.hashCode()
+        result = 31 * result + model.hashCode()
+        result = 31 * result + manufacturer.hashCode()
+        result = 31 * result + pilots.contentHashCode()
         result = 31 * result + films.contentHashCode()
         return result
     }
