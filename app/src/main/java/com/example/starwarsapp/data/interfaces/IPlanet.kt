b176eb4716cabe27fe0fc781c858d56db2d32912
Slava <kruchkovenko@gmail.com>
2023-07-24 21:55:49 +0500

    add planet model

---
+++ app/src/main/java/com/example/starwarsapp/data/interfaces/IPlanet.kt
@@ -0,0 +1,11 @@
+package com.example.starwarsapp.data.interfaces
+
+import com.example.starwarsapp.data.models.Movie
+
+interface IPlanet {
+    val id: String
+    val name: String
+    val diameter: Int
+    val population: Long
+    val films: Array<Movie>
+}
---
+++ app/src/main/java/com/example/starwarsapp/data/models/Planet.kt
@@ -0,0 +1,42 @@
+package com.example.starwarsapp.data.models
+
+import com.example.starwarsapp.data.interfaces.IPlanet
+import com.example.starwarsapp.data.interfaces.Likeable
+
+data class Planet(
+    override val id: String,
+    override val name: String,
+    override val diameter: Int,
+    override val population: Long,
+    override val films: Array<Movie>,
+    private val isLiked: Boolean = false
+) : StarWarsObject(id), IPlanet, Likeable {
+    var isLike = isLiked
+        private set
+
+    override fun dislike() {
+        isLike = false
+    }
+
+    override fun like() {
+        isLike = true
+    }
+
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        return id == (other as Planet).id
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + name.hashCode()
+        result = 31 * result + diameter
+        result = 31 * result + population.hashCode()
+        result = 31 * result + films.contentHashCode()
+        return result
+    }
+
+}
