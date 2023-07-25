deed8390638bfafcb4ce0dec26fb5cea01b93bbb
Slava <kruchkovenko@gmail.com>
2023-07-25 14:54:50 +0500

    init favourite fragment

---
+++ app/src/main/java/com/example/starwarsapp/ui/favourite/FavouriteFragment.kt
@@ -0,0 +1,22 @@
+package com.example.starwarsapp.ui.favourite
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.starwarsapp.databinding.FragmentFavouriteBinding
+
+class FavouriteFragment : Fragment() {
+    private var _binding: FragmentFavouriteBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel by viewModels<FavouriteViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/example/starwarsapp/ui/favourite/FavouriteViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.starwarsapp.ui.favourite
+
+import androidx.lifecycle.ViewModel
+
+class FavouriteViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
